200fcf0a2a5cd59d13c60ec94e61a593b8f5e2a6
3PointCode <tomasz.piwoda@gmail.com>
2026-04-13 15:45:44 +0200

    LeafNode object has been added and tested

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,8 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_props_eq(self):
         props = {
             "href": "https://www.google.com",
@@ -30,6 +30,23 @@ class TestTextNode(unittest.TestCase):
         node = HTMLNode(props=props)
         output = node.props_to_html()
         self.assertEqual(output, ' href="https://www.google.com" target="_nada" custom="something"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello!")
+        self.assertEqual(node.to_html(), "<b>Hello!</b>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click it!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click it!</a>')
+
+    def test_leaf_node_without_tag(self):
+        node = LeafNode(tag=None, value="Hello")
+        self.assertEqual(node.to_html(), "Hello")
 
 if __name__ == "__main__":
     unittest.main()
