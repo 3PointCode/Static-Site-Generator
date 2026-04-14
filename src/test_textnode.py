82551a56959511e70ed6eed4aa27502513d2dfee
3PointCode <tomasz.piwoda@gmail.com>
2026-04-14 20:13:34 +0200

    Add text_node_to_html_node function that transforms the text node into html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -21,6 +21,25 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a different node", TextType.ITALIC)
         node2 = TextNode("This is a different node", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+    
+    def test_a(self):
+        node = TextNode("This is a node", TextType.LINK, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a node")
+        self.assertEqual(html_node.props, {"href": "www.google.com"})
 
 if __name__ == "__main__":
     unittest.main()
